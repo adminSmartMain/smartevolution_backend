ff86717926d6025b76a507a17a2b97a6290c02f3
Luis Molina <luisjose0317@gmail.com>
2025-11-07 21:20:05 -0500

    nueva configuracion servidor produccion 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-set -e
+
 cd apps/base/scripts/pdf_parser/
 rm -rf node_modules
 npm install puppeteer --save
 cd && cd /app
 mkdir -p /app/logs
+
 # Configurar crontab
 python manage.py migrate
 python manage.py crontab add
+
 # Iniciar cron
 service cron start
+
 # Verificar si los crontabs están configurados
 echo "Crontabs configurados:"
-crontab -l || echo "No hay crontab configurados"
+crontab -l
+
 # Ejecutar el servidor Django
-echo "Starting Django server with Gunicorn..."
-exec gunicorn -c gunicorn.conf.py core.wsgi:application
+echo "Starting Django server..."
+exec python manage.py runserver 0.0.0.0:8000
